3017724d147a87d9b60e007e55954d16c88ed8e2
Manuel Benini <manuelbenini1905@gmail.com>
2022-09-26 10:54:24 +0200

    Terminata milestone #1

---
+++ Event.cs
@@ -0,0 +1,159 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Event
+{
+    string title;
+    public string Title
+    {
+        get => title;
+        set
+        {
+            try
+            {
+                if (value == "")
+                {
+                    title = "Undefined title";
+                    throw new Exception("Non è possibile inserire un titolo vuoto");
+                }
+                else
+                {
+                    title = value;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+    DateTime date;
+    public DateTime Date
+    {
+        get => date;
+        set
+        {
+            try
+            {
+                if (value < DateTime.Now)
+                {
+                    throw new Exception("La data dell'evento non può essere precedente alla data attuale");
+                }
+                else
+                {
+                    date = value;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+           
+        }
+    }
+
+    int maxCapacity;
+
+    public int MaxCapacity
+    {
+        get => maxCapacity;
+
+        private set
+        {
+            try
+            {
+                if (value < 0)
+                {
+                    throw new Exception("La capacità massima dell'evento non può essere minore di 0");
+                }
+                else
+                {
+                    maxCapacity = value;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    public int bookedSeats;
+
+    public int BookedSeats
+    {
+        get => bookedSeats;
+
+        private set
+        {
+            bookedSeats = value;
+        }
+    }
+
+    public Event(string title, DateTime date, int maxCapacity)
+    {
+        Title = title;
+        Date = date;
+        MaxCapacity = maxCapacity;
+        BookedSeats = 0;
+    }
+
+    public void BookSeats(int n)
+    {
+        try
+        {
+            if (Date < DateTime.Now)
+            {
+                throw new Exception("La data dell'evento non può essere precedente alla data attuale");
+            }
+            else if (BookedSeats >= maxCapacity)
+            {
+                throw new Exception("Non ci sono più posti prenotabili per l'evento");
+            }
+            else if(n > MaxCapacity)
+            {
+                throw new Exception("I posti che si vogliono prenotare superano la capacità massima dell'evento");
+            }
+            else if(n > maxCapacity - BookedSeats)
+            {
+                throw new Exception("Si vogliono prenotare più posti di quanti ce ne siano disponibili");
+            }
+            else
+            {
+                BookedSeats += n;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        
+    }
+
+    public void CancelSeats(int n)
+    {
+        try
+        {
+            if (Date < DateTime.Now)
+            {
+                throw new Exception("La data dell'evento non può essere precedente alla data attuale");
+            }
+            else if (n > BookedSeats)
+            {
+                throw new Exception("Non è possibile disdire più posti di quanto ce ne siano prenotati");
+            }
+            else
+            {
+                BookedSeats -= n;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+    public override string ToString()
+    {
+        string dateAndTitle = Date.ToString("dd/MM/yyyy") + " - " + Title;
+        return dateAndTitle;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+Event newEvent = new Event("Rockstar", new DateTime(2025, 3, 1, 7, 0, 0), 50);
+
+Console.WriteLine(newEvent.Title);
+Console.WriteLine(newEvent.Date);
+Console.WriteLine(newEvent.MaxCapacity);
+Console.WriteLine(newEvent.ToString());
+
+newEvent.BookSeats(50);
+Console.WriteLine(newEvent.BookedSeats);
+
+newEvent.CancelSeats(50);
+Console.WriteLine(newEvent.BookedSeats);
