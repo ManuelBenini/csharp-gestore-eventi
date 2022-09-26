10a068f4021cb4c5bf763dacbf1003a6dc169c7c
Manuel Benini <manuelbenini1905@gmail.com>
2022-09-26 12:11:39 +0200

    Terminata milestone #3

--- Program.cs
+++ Program.cs
@@ -1,16 +1,34 @@
 ﻿
-Console.Write("Salve! Inserisci il nome dell'evento: ");
-string eventTitle = Console.ReadLine();
+//Console.Write("Salve! Inserisci il nome dell'evento: ");
+//string eventTitle = Console.ReadLine();
 
-Console.Write("Inserisci data dell'evento (gg/mm/yyyy): ");
-DateTime eventDate = Convert.ToDateTime(Console.ReadLine());
+//Console.Write("Inserisci data dell'evento (gg/mm/yyyy): ");
+//DateTime eventDate = Convert.ToDateTime(Console.ReadLine());
 
-Console.Write("Inserisci numero di posti totali: ");
-int eventCapacity = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Inserisci numero di posti totali: ");
+//int eventCapacity = Convert.ToInt32(Console.ReadLine());
 
-Event newEvent = new Event(eventTitle, eventDate, eventCapacity);
+//Event newEvent = new Event(eventTitle, eventDate, eventCapacity);
 
-Console.Write("Quanti posti desideri prenotare? ");
-newEvent.BookSeats(Convert.ToInt32(Console.ReadLine()));
+//Console.Write("Quanti posti desideri prenotare? ");
+//newEvent.BookSeats(Convert.ToInt32(Console.ReadLine()));
 
-Console.WriteLine(newEvent.BookedSeats);
+//Console.WriteLine(newEvent.BookedSeats);
+
+ProgramEvents newProgram = new ProgramEvents("Giochi d'autunno");
+
+newProgram.EventAdder(new Event("Rockstar", new DateTime(2023, 3, 1, 7, 0, 0), 150));
+
+newProgram.EventAdder(new Event("Barbablù", new DateTime(2023, 3, 1, 7, 0, 0), 150));
+
+newProgram.EventPrintOnDate(new DateTime(2023, 3, 1, 7, 0, 0));
+
+newProgram.EventsPrint();
+
+Console.WriteLine(newProgram.NumberOfEvents());
+
+newProgram.ProgramPrint();
+
+newProgram.EventsClear();
+
+Console.WriteLine(newProgram.NumberOfEvents());
---
+++ ProgramEvents.cs
@@ -0,0 +1,62 @@
+﻿
+public class ProgramEvents
+{
+    public string Title { get; set; }
+
+    public List<Event> Events;
+
+    public ProgramEvents(string title)
+    {
+        Title = title;
+        Events = new List<Event>();
+    }
+
+    public void EventAdder(Event newEvent)
+    {
+        Events.Add(newEvent);
+    }
+    public void EventPrintOnDate(DateTime date)
+    {
+        bool eventsExistOnDate = false;
+
+        foreach (Event e in Events)
+        {
+            if(e.Date == date)
+            {
+                Console.WriteLine(e.Title);
+                eventsExistOnDate = true;
+            }
+        }
+
+        if (!eventsExistOnDate)
+        {
+            Console.WriteLine("Non esistono eventi nella data scelta.");
+        }
+        
+    }
+
+    public void EventsPrint()
+    {
+        foreach (Event e in Events)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    public int NumberOfEvents()
+    {
+        return Events.Count;
+    }
+
+    public void EventsClear()
+    {
+        Events.Clear();
+    }
+
+    public void ProgramPrint()
+    {
+        Console.WriteLine($"Eventi presenti nel programma: {Title}");
+        EventsPrint();
+    }
+
+}
